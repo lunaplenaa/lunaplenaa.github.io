e8d53cd192c89e1c330ccca85661e3eebdffa4a0
lunaplenaa <lunacarmina25@gmail.com>
2025-07-29 10:52:59 -0500

    Update serverside.php

--- serverside.php
+++ serverside.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST['message']);
     $file = 'messages.txt';
     $current_messages = file_get_contents($file);
-    $current_messages .= "<div class='message'><p><strong>Anonymous:</strong> $message</p></div>\n";
+    $current_messages .= "<div class='message'><p><strong>anon says:</strong> $message</p></div>\n";
     file_put_contents($file, $current_messages);
 
     header("Location: index.html");
