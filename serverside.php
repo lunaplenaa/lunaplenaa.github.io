8dd91c144a5c8bcdc1c1e2bf88190683af082e96
lunaplenaa <lunacarmina25@gmail.com>
2025-07-29 10:52:24 -0500

    Update serverside.php

--- serverside.php
+++ serverside.php
@@ -1,6 +1,18 @@
 <?php
-    $receivedData = $_POST['data_to_send'];
-    // Process data, then redirect to another page or render content
-    header("Location: answers.html?data=" . urlencode($receivedData));
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the submitted message from the form
+    $message = htmlspecialchars($_POST['message']);
+
+    // Here, you could save the message to a database, a file, etc.
+    // For simplicity, we'll save it to a file
+
+    $file = 'messages.txt';
+    $current_messages = file_get_contents($file);
+    $current_messages .= "<div class='message'><p><strong>Anonymous:</strong> $message</p></div>\n";
+    file_put_contents($file, $current_messages);
+
+    // Redirect to the original page to show the new message
+    header("Location: index.html");
     exit();
-    ?>
+}
+?>
